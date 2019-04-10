da897eba3fba0cdabbe05c80152349bbf46de1ae
Sam <sam.d.odonnell@gmail.com>
2019-04-09 18:50:13 -0700

    Add caribbean data

---
+++ dist/data/caribbean.js
@@ -0,0 +1,644 @@
+caribbeanChildren = [
+    {
+    "name": "chicken",
+    "children": [{
+        "name": "green beans",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "cauliflower",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "sweet potatoes",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "zucchini",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "bell peppers",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "tomatoes",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }]
+}, {
+    "name": "pork",
+    "children": [{
+        "name": "green beans",
+        "children": [{
+            "name": "curry powder",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "cauliflower",
+        "children": [{
+            "name": "curry powder",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "sweet potatoes",
+        "children": [{
+            "name": "curry powder",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "zucchini",
+        "children": [{
+            "name": "curry powder",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "bell peppers",
+        "children": [{
+            "name": "curry powder",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "tomatoes",
+        "children": [{
+            "name": "curry powder",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }]
+}, {
+    "name": "fish",
+    "children": [{
+        "name": "green beans",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }]
+    }, {
+        "name": "cauliflower",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }]
+    }, {
+        "name": "sweet potatoes",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }]
+    }, {
+        "name": "zucchini",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }]
+    }, {
+        "name": "bell peppers",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }]
+    }, {
+        "name": "tomatoes",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "hot peppers",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "chillies",
+            "size": 1
+        }]
+    }]
+}, {
+    "name": "chickpeas",
+    "children": [{
+        "name": "green beans",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "cauliflower",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "sweet potatoes",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "zucchini",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "bell peppers",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }, {
+        "name": "tomatoes",
+        "children": [{
+            "name": "basil",
+            "size": 1
+        }, {
+            "name": "oregano",
+            "size": 1
+        }, {
+            "name": "allspice",
+            "size": 1
+        }, {
+            "name": "paprika",
+            "size": 1
+        }, {
+            "name": "cumin",
+            "size": 1
+        }, {
+            "name": "ginger",
+            "size": 1
+        }, {
+            "name": "saffron",
+            "size": 1  
+        }, {
+            "name": "chillies",
+            "size": 1
+        }, {
+            "name": "jerk spices",
+            "size": 1
+        }]
+    }]
+}]
